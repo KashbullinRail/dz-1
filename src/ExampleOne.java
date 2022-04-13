6e998771c55683c7639ccc9357fab2e59f294a77
KashbullinRail <kashbullinrail@gmail.com>
2022-04-13 21:31:43 +0300

    MY first commit

---
+++ src/ExampleOne.java
@@ -0,0 +1,12 @@
+/**
+ * SkyPro. My code dz#1
+ *
+ * @author Раиль
+ * @version dated Apr 13, 2022
+ */
+
+public class ExampleOne {
+    public static void main(String[] args) {
+        System.out.println("Hello SkyPro!");
+    }
+}
